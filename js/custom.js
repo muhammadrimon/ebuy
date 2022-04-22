010a2761c89555af5b18332963a41dac557880e8
muhammadrimon <course.creativeit@gmail.com>
2022-04-21 21:47:12 -0600

    brand slider done

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  "use strict";
+
+  //   branding slider;
+  $(".brand-slider").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa-solid fa-chevron-left slick-prev left-arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right slick-next right-arrow"></i>',
+    centerMode: true,
+    centerPadding: "0px",
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
