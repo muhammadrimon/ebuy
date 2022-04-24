2dbba2ee196cdd9cd9d7da538c0557c443751de1
muhammadrimon <course.creativeit@gmail.com>
2022-04-24 01:01:40 -0600

    banner slider and card ends

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   "use strict";
 
+  // banner-slider;
+
+  $(".banner-slider").slick({
+    prevArrow: '<i class="fa-solid fa-chevron-left slick-prev left-arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right slick-next right-arrow"></i>',
+  });
+
   //   branding slider;
   $(".brand-slider").slick({
     slidesToShow: 5,
