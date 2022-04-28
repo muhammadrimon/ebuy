04f96f55b8fa8f024e0dcefdd93c5655f661ae80
muhammadrimon <course.creativeit@gmail.com>
2022-04-28 03:08:48 -0600

    product details slider done

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,26 @@ $(document).ready(function () {
     nextArrow: '<i class="fa-solid fa-chevron-right slick-next right-arrow"></i>',
   });
 
+  // preview slider;
+
+  $(".preview-slider").slick({
+    arrows: false,
+    fade: true,
+    draggable: false,
+    asNavFor: ".thumbnail-slider",
+  });
+
+  // thumbnail slider;
+
+  $(".thumbnail-slider").slick({
+    arrows: false,
+    slidesToShow: 3,
+    focusOnSelect: true,
+    centerMode: true,
+    centerPadding: "0px",
+    asNavFor: ".preview-slider",
+  });
+
   //   branding slider;
   $(".brand-slider").slick({
     slidesToShow: 5,
